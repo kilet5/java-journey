6177e62a5061a7bff7ddf73cfd82763bd4dfc202
kilet5 <124427964+kilet5@users.noreply.github.com>
2025-07-09 00:16:30 +0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hi java");
+        System.out.println("RPO JAVA 2");
+        
+        int currentYear = 2025;
+        int studyYears = 2;
+        int gradYear = currentYear + (4 - studyYears);
+
+        System.out.println("Year end: " + gradYear);
+
+    }
+}
